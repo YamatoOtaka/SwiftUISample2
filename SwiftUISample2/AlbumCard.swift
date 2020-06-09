7e9dabd34662ca1f9d36b9ad882df38f3cc3b268
Yamato Otaka <yamato8010apple@gmail.com>
2020-06-09 11:49:13 +0900

    Add AlbumCard

---
+++ SwiftUISample2/AlbumCard.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumCard.swift
+//  SwiftUISample2
+//
+//  Created by 大高倭 on 2020/06/09.
+//  Copyright © 2020 YamatoOtaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct AlbumCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumCard_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumCard()
+    }
+}
