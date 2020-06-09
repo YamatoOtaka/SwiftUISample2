d07d7fad8fd4ad1dbef0b936ef8d09f197bdc327
Yamato Otaka <yamato8010apple@gmail.com>
2020-06-09 11:39:22 +0900

    Initial commit

---
+++ SwiftUISample2/AlbumListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISample2
+//
+//  Created by 大高倭 on 2020/06/08.
+//  Copyright © 2020 YamatoOtaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
